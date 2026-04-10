35a74018a1241ec8b2bc928ab3237dbac45b5266
KeremParali <rg22mkmvrq@privaterelay.appleid.com>
2026-04-10 21:52:04 +0300

    Rename SORU6.cpp to Tanımlama Kuralları.cpp

---
+++ Tanımlama Kuralları.cpp
@@ -0,0 +1,45 @@
+/****************************************************************************************
+**
+**  İSTANBUL 29 MAYIS ÜNİVERSİTESİ
+**  BİLİŞİM TEKNOLOJİLERİ MESLEK YÜKSEKOKULU
+**  BÜYÜK VERİ ANALİSTLİĞİ PR.
+**  PROGRAMLAMAYA GİRİŞ DERSİ
+**  2025-2026 GÜZ DÖNEMİ
+**
+**  ÖDEV NUMARASI : SORU 6
+**  ÖĞRENCİ ADI SOYADI : Kerem PARALI
+**  ÖĞRENCİ NUMARASI : 300125024
+**
+****************************************************************************************/
+
+
+/***************************** DEĞİŞKEN TANIMLAMA KURALLARI ************************
+****************** 2 DOĞRU TANIM / 3 YANLIŞ TANIM ÖRNEĞİ **************************/
+
+
+/*************************** DOĞRU TANIMLAMALAR **************************/
+
+
+int ogrenciSayisi;
+// Değişken ismi harf ile başlar ve boşluk içermez
+
+double sinavOrt;
+// Anahtar kelime değil, anlamlı bir isim kullanılmış
+
+char notHarf;
+// Sayı ve harf birlikte kullanılabilir
+
+
+
+/************************** YANLIŞ TANIMLAMALAR: **************************/
+
+
+int 1sayi;
+// Değişken ismi sayı ile başlayamaz
+
+float toplam - not;
+// Değişken isminde - (eksi) kullanılamaz
+
+int float;
+// Anahtar kelimeler değişken ismi olamaz
+
