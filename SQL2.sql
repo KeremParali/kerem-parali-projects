c73bfb16b565db2292f003818787786d9b916422
KeremParali <rg22mkmvrq@privaterelay.appleid.com>
2026-04-10 22:29:52 +0300

    Create SQL2.sql

---
+++ SQL2.sql
@@ -0,0 +1,13 @@
+INSERT INTO gosterim(film_id, salon_id, gosterim_tarihi, seans, bilet_fiyati) VALUES /*---> INSERT INTO Tabloya veri eklemek için(Values = Değer)*/
+(1, 1, '2026-05-10', 'Akşam', 220),
+(2, 3, '2026-05-10', 'Akşam', 250),
+(3, 2, '2026-05-11', 'Öğle', 180),
+(4, 4, '2026-05-11', 'Akşam', 200),
+(5, 1, '2026-05-12', 'Akşam', 230),
+(6, 3, '2026-05-12', 'Sabah', 170);
+
+INSERT INTO yazar(ad_soyad, ulke, dogum_yili, biyografi) VALUES    /*--------> INSERT INTO Tabloya veri eklemek için(Values = Değer)*/
+('Christopher Nolan', 'İngiltere', 1970, 'Bilim kurgu ve gerilim filmleriyle tanınır'),
+('Nuri Bilge Ceylan', 'Türkiye', 1959, 'Dram türünde ödüllü yönetmen'),
+('Greta Gerwig', 'ABD', 1983, 'Modern Hollywood yönetmeni ve senarist'),
+('Bong Joon-ho', 'Güney Kore', 1969, 'Oscar ödüllü yönetmen');
